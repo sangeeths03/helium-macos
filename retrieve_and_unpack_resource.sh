12c211f211832a9d71ee21641fd35798b0908362
jj <log@riseup.net>
2025-03-04 19:49:19 +0000

    retrieve_and_unpack_resource.sh: fix botched path from merge
    oops

--- retrieve_and_unpack_resource.sh
+++ retrieve_and_unpack_resource.sh
@@ -55,9 +55,9 @@ if $retrieve_generic; then
 
     # Retrieve and unpack general resources
     python3 "$_main_repo/utils/downloads.py" retrieve -i "$_root_dir/downloads.ini" -c "$_download_cache"
-    python3 "$_main_repo/utils/downloads.py" retrieve -i "$_root_dir/extras.ini" -c "$_download_cache"
+    python3 "$_main_repo/utils/downloads.py" retrieve -i "$_main_repo/extras.ini" -c "$_download_cache"
     python3 "$_main_repo/utils/downloads.py" unpack -i "$_root_dir/downloads.ini" -c "$_download_cache" "$_src_dir"
-    python3 "$_main_repo/utils/downloads.py" unpack -i "$_root_dir/extras.ini" -c "$_download_cache" "$_src_dir"
+    python3 "$_main_repo/utils/downloads.py" unpack -i "$_main_repo/extras.ini" -c "$_download_cache" "$_src_dir"
 fi
 
 if $retrieve_arch_specific; then
